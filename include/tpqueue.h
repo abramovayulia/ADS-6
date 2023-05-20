1fd255a8e9f9e0b96003437ad6ffef0796c054dd
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-05-20 22:11:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,72 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <cassert>
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+ private:
+  T* arr;
+  int sizeM;
+  int first, last;
+  int count;
 
+ public:
+  TPQueue():sizeM(size), first(0), last(0), count(0) {
+    arr = new T[sizeM + 1];
+  }
+
+  ~TPQueue() {
+    delete[] arr;
+  }
+
+  void push(const T& value) {
+    assert(count < sizeM);
+    if (count == 0) {
+      arr[last++] = value;
+      count++;
+    } else {
+      int i = last - 1;
+      bool flag = 0;
+      while (i >= first && value.prior > arr[i].prior) {
+        flag = 1;
+        arr[i + 1] = arr[i];
+        arr[i] = value;
+        i--;
+      }
+      if (flag == 0) {
+        arr[last] = value;
+      }
+      last++;
+      count++;
+    }
+    if (last > sizeM) {
+      last -= sizeM + 1;
+    }
+  }
+
+  const T& pop() {
+    assert(count > 0);
+    count--;
+    if (first > sizeM) {
+      first -= sizeM + 1;
+    }
+    return arr[first++];
+  }
+
+  const T& get() {
+    assert(count > 0);
+    return arr[first];
+  }
+
+  bool isFull() const {
+    return count == sizeM;
+  }
+
+  bool isEmpty() const {
+    return count == 0;
+  }
+};
 struct SYM {
   char ch;
   int prior;
